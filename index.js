5df5180b62567556cd542c713797a7f694e3ebcd
VIktorPls <gabriel.vides96@gmail.com>
2022-08-22 21:42:21 -0300

    db connected

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const mongoose = require("mongoose");
 
-app.use(morgan('combined'))
+require("dotenv").config();
+// console.log(process.env)
 
-app.get('/', (req, res)=>{
- res.send('<h1>Hello world!</h1>') 
-})
+mongoose.connect(process.env.DB_CONNECT, () => console.log("DB Connected"));
 
-app.listen(3000)
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world!</h1>");
+});
+
+app.listen(3000, () => console.log("Server up and running"));
