b3d36ad1ed33f00cbde10e7647ce72bbb1beb9bb
VIktorPls <gabriel.vides96@gmail.com>
2022-09-01 15:22:50 -0300

    ADD validations to register

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const app = express();
 const mongoose = require("mongoose");
 const User = require("./model/User");
+const { body, validationResult } = require("express-validator");
 
 require("dotenv").config();
 
@@ -10,12 +11,6 @@ mongoose.connect(process.env.DB_CONNECT, () => console.log("DB Connected"));
 app.use(express.json());
 app.use(morgan("combined"));
 app.use(express.static("public"));
-
-// ============ Utils ============ //
-const isValidEmail = (input) => {
-
-  return false;
-};
 
 // ============ Routes ============ //
 //Home
@@ -28,19 +23,41 @@ app.get("/register", (req, res) => {
   res.sendFile(__dirname + "/public/register.html");
 });
 
-app.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
+app.post(
+  "/register",
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  const newUser = new User({ name, email, password });
+    const { name, email, password } = req.body;
 
-  if (name && email && isValidEmail(email) && password) {
+    const newUser = new User({ name, email, password });
+
+    //FIXME: Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client.
+    /* if (newUser.email) {
+      User.findOne({ email: newUser.email }).then((user) => {
+        console.log(user);
+        return res.send({ msg: "Email already exists" });
+      });
+    } */
+
+  if (name && email && password) {
     await newUser
       .save()
-      .then((user) => res.json(user))
-      .catch((error) => {throw error});
+      .then((user) => res.status(201).json(user))
+      .catch((error) => {
+        throw error;
+      });
+      return
   }
 
-  res.send("Something went wrong!");
-});
+    return res.send("Something went wrong!");
+  }
+);
 
 app.listen(3000, () => console.log("Server up and running"));
